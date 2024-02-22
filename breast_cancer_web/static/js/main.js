daaed71f80f0e3d0e96e719852bd0cf84614f27b
LittleHaku <ivandelhorno@gmail.com>
2024-02-22 12:25:46 +0100

    return to top

--- breast_cancer_web/static/js/main.js
+++ breast_cancer_web/static/js/main.js
@@ -1,17 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("script loaded");
 
-    $(".details__summary").click(function(){
+    $(".details__summary").click(function () {
         var content = $(this).next();
         if (content.css('max-height') != '0px') {
             content.css('max-height', '0px');
         } else {
-            setTimeout(function() {
+            setTimeout(function () {
                 content.css('max-height', content.prop('scrollHeight') + 'px');
             }, 0);
             $('html, body').animate({
                 scrollTop: $(this).offset().top
             }, 500);
         }
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {
+            $('#go-top').fadeIn(200);
+        } else {
+            $('#go-top').fadeOut(200);
+        }
+    });
+    $('#go-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
     });
 });
