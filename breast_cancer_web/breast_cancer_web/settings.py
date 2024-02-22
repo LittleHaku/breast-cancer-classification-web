4ce096685dc4a94dc743c93479e1721714c292ab
LittleHaku <ivandelhorno@gmail.com>
2024-02-23 00:24:50 +0100

    local host y debug

--- breast_cancer_web/breast_cancer_web/settings.py
+++ breast_cancer_web/breast_cancer_web/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-271p_fl$*i2^^s2b2m%#g%71*a#i@(k0_w*e1ub8p9c6d9%(g-"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["breast-cancer-classification-web.onrender.com", "localhost"]
 
 
 # Application definition
