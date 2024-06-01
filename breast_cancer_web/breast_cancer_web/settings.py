96e5a3476403622ac7385edbd548768807a9aea5
LittleHaku <ivandelhorno@gmail.com>
2024-06-01 16:06:07 +0200

    closing bracket...

--- breast_cancer_web/breast_cancer_web/settings.py
+++ breast_cancer_web/breast_cancer_web/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["breastcancerclassifiers.up.railway.app", "localhost", "breast-cancer-classification-web.onrender.com", "127.0.0.1"
+ALLOWED_HOSTS = ["breastcancerclassifiers.up.railway.app", "localhost", "breast-cancer-classification-web.onrender.com", "127.0.0.1"]
 
 
 # Application definition
